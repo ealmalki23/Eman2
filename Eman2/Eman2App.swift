0305f53a8f5a23fbb94a59321d57932aec156796
ealmalki23 <147074692+ealmalki23@users.noreply.github.com>
2023-10-08 14:42:13 +0300

    Initial Commit

---
+++ Eman2/Eman2App.swift
@@ -0,0 +1,17 @@
+//
+//  Eman2App.swift
+//  Eman2
+//
+//  Created by Eman Almalki  on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Eman2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
